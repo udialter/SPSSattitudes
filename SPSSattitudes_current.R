c7a385918d301018d080c886782e447e98d40625
carmendang <dangcrmn@gmail.com>
2020-08-09 22:34:24 -0400

    general sectioning script

---
+++ SPSSattitudes_current.R
@@ -0,0 +1,19 @@
+#' ---
+#' title: "SPSS Attitudes Measure"
+#' author: "Carmen Dang"
+#' date: "August 9th, 2020"
+#' output: github_document
+#' ---
+#'
+#' Load and clean data
+#' 
+#' Descriptive statistics
+#' 
+#' Split into EFA/CFA sets
+#' 
+#' Polychoric correlation
+#' 
+#' MAP / Parallel Analysis
+#' 
+#' EFAs 
+#' 
